478fd5eac5a42160d505abc1b00bd896ec06688f
nassifdu <dudugrtuio@gmail.com>
2022-10-06 19:33:00 -0300

    pegando dados do openweather

--- clima.js
+++ clima.js
@@ -5,3 +5,41 @@ const timezone = document.getElementById('time-zone');
 const countryEl = document.getElementById('country');
 const weatherForecastEl = document.getElementById('weather-forecast');
 const currentTempEl = document.getElementById('current-temp');
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+const apiKey = '7979fcc1fcf2c82468730a221b5d599b';
+
+// datas e temporizador
+setInterval(() => {
+  const time = new Date();
+  const month = time.getMonth();
+  const date = time.getDate();
+  const day = time.getDay();
+  const hour = time.getHours();
+  const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+  const minutes = time.getMinutes();
+  const ampm = hour >= 12 ? 'PM' : 'AM'
+
+  timeEl.innerHTML = (hoursIn12HrFormat < 10 ? '0'+
+        hoursIn12HrFormat : hoursIn12HrFormat) + ':' +
+        (minutes < 10? '0'+minutes: minutes)+ ' ' + `<span id="am-pm">
+        ${ampm}</span>`
+  dateEl.innerHTML = days[day] + ', ' + date+ ' ' + months[month]
+
+}, 1000);
+
+function getWeatherData(){
+    navigator.geolocation.getCurrentPosition((success)=>{
+        console.log(success);
+        let{latitude,longitude} = success.coords;
+        
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${apiKey}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+    })
+}
+
+getWeatherData();
